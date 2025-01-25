f08f06ccf8afb6fc14c3a881e1e76dc1189b8832
ey1410 <ericy1410@gmail.com>
2025-01-25 08:51:40 -0800

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+// helloworld.java
+//1/25/25
+
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        //print "hello World!" message to the console
+        System.out.println("Hello world!!");
+    }
+}
